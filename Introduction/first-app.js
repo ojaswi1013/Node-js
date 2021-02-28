099d95ed245082ada7aaaf20a7ee95ac24e4ac8f
Ojaswi Singh <singh.ojaswi1013@gmail.com>
2021-02-28 14:25:46 +0530

    Created first app and used basic commands

---
+++ Introduction/first-app.js
@@ -0,0 +1,6 @@
+//learning some basic commands in node-js
+
+console.log('Hello from Node-js');
+
+const fs = require('fs'); //fs stands for file system for some functionalities from node
+fs.writeFileSync('Hello.txt','hello from node-js'); //writing into a file
