bdcda1bbec886f8268dc5d79854c65e996b7f101
Ojaswi Singh <singh.ojaswi1013@gmail.com>
2021-03-03 01:44:25 +0530

    express-js basics

---
+++ 4Express/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/add-product', (req,res,next) => {
+    console.log('In another middleware!');
+    res.send('<h1>the "Add-Product" page</h1>');
+});
+app.use('/', (req,res,next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from express!</h1>');
+});
+
+app.listen(3000);
