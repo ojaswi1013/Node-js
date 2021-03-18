e62360b1a5a295ef40b70082ddbdfd8840ef2055
Ojaswi Singh <singh.ojaswi1013@gmail.com>
2021-03-18 21:32:48 +0530

    added new features to product and site as well

--- 6MVCimplement/controllers/admin.js
+++ 6MVCimplement/controllers/admin.js
@@ -1,11 +1,5 @@
 const Product = require('../models/product');
 
-exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
-}
-
 // /admin/add-product => GET
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', {
@@ -15,6 +9,16 @@ exports.getAddProduct = (req, res, next) => {
         productCSS: true,
         activeAddProduct: true
     });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const imageURL = req.body.imageURL;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageURL, description, price);
+    product.save();
+    res.redirect('/');
 }
 
 //  /admin/products => GET
